17e17788921500ab01cac7d01a96e560834ceaaa
hamzaali <hamza54834@gmail.com>
2020-05-06 14:58:41 -0700

    Events in Complete

--- src/Events.js
+++ src/Events.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-
+import Counter from './Counter'
 
 class MyEvents extends Component{
 constructor(props){
@@ -8,19 +8,48 @@ super(props);
     this.state={
         counter: 0
     }
+    this.incrementCounter=this.incrementCounter.bind(this);
+    this.decrementCounter=this.decrementCounter.bind(this);
 // this.incrementCounter=this.incrementCounter.bind(this);
 }
 // Click Increment
-incrementCounter(param,ev){
-    // console.log(this,'this');
-    console.log('ev',ev)
-    console.log('Param',param)
-this.setState({
-    counter: this.state.counter+1
-})
-}  
+// incrementCounter(param,ev){
+//     // console.log(this,'this');
+//     // console.log('ev',ev)
+//     // console.log('Param',param)
+// this.setState({
+//     counter: this.state.counter+1
+// })
+// }  
 
- 
+
+incrementCounter(){
+// console.log('increment');
+// this.setState({
+    // counter: this.state.counter+1
+    if(this.state.counter < 11){
+ this.setState({counter: this.state.counter+1})
+    }
+    else{
+console.log('already reached maximum count') 
+    }
+// })
+}
+
+ decrementCounter(){
+// console.log('decrement');
+// this.setState({
+    if(this.state.counter === 0){
+console.log('decrement count reached')
+    }
+    else{
+        this.setState({counter: this.state.counter+ 1})
+
+    }
+    // counter: this.state.counter - 1
+
+// })
+}
 
 //     clickHandler(){
 //     console.log('click pressed')
@@ -36,12 +65,16 @@ this.setState({
                console.log('first button')
            }}>Click me</button> */}
            {/* <button onClick={this.clickHandler}>Click me</button> */}
-<h1>Hello Events</h1>
+{/* <h1>Hello Events</h1>
            {/* <button onClick={this.incrementCounter.bind(this)}>+</button><br /> */}
-<button onClick={this.incrementCounter.bind(this,'Hello')}>Button Hello</button>
-           <span>{this.state.counter}</span>
+{/* <button onClick={this.incrementCounter.bind(this,'Hello')}>Button Hello</button> */}
+           {/* <span>{this.state.counter}</span> */}
        {/* <button onClick={this.incrementCounter}>+</button> */}
-       <button onClick={this.incrementCounter.bind(this,'Events')}></button>
+       {/* <button onClick={this.incrementCounter.bind(this,'Events')}></button> */} 
+
+
+<Counter counter={this.state.counter} increment={this.incrementCounter} decrement={this.decrementCounter} />
+
        </div>
         
     )
