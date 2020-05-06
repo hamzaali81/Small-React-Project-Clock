9caa4e600a34d0c855b0a1205a93aae91fe1cb3b
hamzaali <hamza54834@gmail.com>
2020-05-06 14:18:33 -0700

    Events in practice

--- src/Events.js
+++ src/Events.js
@@ -8,11 +8,13 @@ super(props);
     this.state={
         counter: 0
     }
-this.incrementCounter=this.incrementCounter.bind(this);
+// this.incrementCounter=this.incrementCounter.bind(this);
 }
 // Click Increment
-incrementCounter(){
+incrementCounter(param,ev){
     // console.log(this,'this');
+    console.log('ev',ev)
+    console.log('Param',param)
 this.setState({
     counter: this.state.counter+1
 })
@@ -36,9 +38,10 @@ this.setState({
            {/* <button onClick={this.clickHandler}>Click me</button> */}
 <h1>Hello Events</h1>
            {/* <button onClick={this.incrementCounter.bind(this)}>+</button><br /> */}
-
+<button onClick={this.incrementCounter.bind(this,'Hello')}>Button Hello</button>
            <span>{this.state.counter}</span>
-       <button onClick={this.incrementCounter}>+</button>
+       {/* <button onClick={this.incrementCounter}>+</button> */}
+       <button onClick={this.incrementCounter.bind(this,'Events')}></button>
        </div>
         
     )
