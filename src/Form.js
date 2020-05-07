720889d9dadf351ef724fa005b5bce527d0819a1
hamzaali <hamza54834@gmail.com>
2020-05-07 12:11:01 -0700

    form

--- src/Form.js
+++ src/Form.js
@@ -6,11 +6,16 @@ class Forms extends Component{
         super(props);
         this.state={
             username: '',
-            allUsers:[]
+            allUsers:[],
+            radioGroup:{
+                MEANSTACK: false,
+                MERNSTACK: true
+            }
         }
         this.formHandler=this.formHandler.bind(this);
     this.clickHandler=this.clickHandler.bind(this);
-    }
+this.radioHandler=this.radioHandler.bind(this);    
+}
     
     formHandler(ev){
 console.log(ev.target.value , 'events' ,ev)
@@ -25,10 +30,33 @@ clickHandler(ev){
  currentUser.push(this.state.username)
  this.setState({
      allUsers: currentUser,
-     username: ''
+     username: '',
+    // radioGroup:{
+    //     MEANSTACK: false,
+    //     MERNSTACK: true
+    // }
+
  })
  console.log(this.state.allUsers)
 }
+
+radioHandler(ev){
+    console.log(ev.target.value)
+    let radioGroup=this.state.radioGroup;
+    for(var key in radioGroup){
+        radioGroup[key]=false
+
+    }
+radioGroup[ev.target.value]=ev.target.checked;
+
+this.setState({
+    radioGroup: radioGroup
+})
+}
+
+
+
+
 
     render(){
         return(
@@ -37,6 +65,17 @@ clickHandler(ev){
     <input type="text" name="username" value={this.state.username} onChange={this.formHandler} />
  <button onClick={this.clickHandler}>Save users</button>
     <p>{this.state.username}</p>
+<br/>
+Developer Category
+<br/>
+<label>
+    Mean stack<input type="radio" name="deveCategory" value="Meanstack" checked={this.state.radioGroup['MEANSTACK']} onChange={this.radioHandler}/>
+</label>
+<br/>
+<label>
+    Mern Stack <input type="radio" name="deveCategory" value="Mernstack" checked={this.state.radioGroup['MERNSTACK']} onChange={this.radioHandler}/>
+</label>
+
 </div>
         )
     }
